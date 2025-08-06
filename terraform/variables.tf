82a45523c314b75a522721354caea4544bfeecdf
Matthijs Baas <matthijs.baas@sogeti.com>
2025-08-06 12:05:56 +0200

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "dtap_environment" {
+  description = "DTAP environment (e.g., dev, tst, acc, prd)"
+  type        = string
+}
+
+variable "subscription_id" {
+  type        = string
+  description = "Azure subscription ID"
+}
