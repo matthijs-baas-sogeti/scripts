7da524ca209c355e9c85ff8a3b04df1af10a53e6
Matthijs Baas <matthijs.baas@sogeti.com>
2025-08-06 18:43:09 +0200

    Delete terraform directory

--- terraform/locals.tf
+++
@@ -1,7 +0,0 @@
-locals {
-  config = yamldecode(file("../config/${var.dtap_environment}.yml"))
-  aks    = local.config["aks"]
-  naming = local.config["naming"]
-  mgmt   = local.config["mgmt"]
-  
-}
--- terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "dtap_environment" {
-  description = "DTAP environment (e.g., dev, tst, acc, prd)"
-  type        = string
-}
-
-variable "subscription_id" {
-  type        = string
-  description = "Azure subscription ID"
-}
